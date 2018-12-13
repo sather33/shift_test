1b0c7b257b084b9faa2aa816b78d4f95b0d0ae18
sather33 <court.dream3@gmail.com>
2018-12-13 14:58:46 +0800

    fixed js for show actived schedule after member is unactived

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -10,27 +10,6 @@
 @yield('script')
 <script type="text/javascript">
     $(document).ready(function(){
-        var table = $('.table');
-        for(i=0; i<table.length; ++i){
-            var lucky = table[i].getElementsByClassName('lucky');
-            var lucky_time = table[i].getElementsByClassName('lucky_time');
-            var edited = $('#input_edit');
-            var is_edited = edited.children().length;
-            for(j=0; j<lucky.length; ++j){
-                var lucky_name = lucky[j].textContent;
-                var shift = lucky_time[j].textContent.split('-');
-                var set_array = table[i].getElementsByClassName(lucky_name)[0];
-                for(k=shift[0]; k<shift[1]; ++k){
-                    set_array.getElementsByClassName(k)[0].classList.add('active');
-                }
-                if((is_edited != '0') && (i == '0')){
-                    var edit_lucky = edited[0].getElementsByClassName(`edit_${lucky_name}`)[0];
-                    edit_lucky.getElementsByClassName('start')[0].value = shift[0];
-                    edit_lucky.getElementsByClassName('end')[0].value = shift[1];
-                }
-            }
-        }
-
         var anchor = $('#anchor').text();
         if(anchor != ''){
             var elmnt = document.getElementById(anchor).parentNode;
@@ -51,6 +30,29 @@
         $('.arrow-down').click(function(){
             $('.mobile-hidden').toggleClass('mobile-show');
         });
+
+        var table = $('.table');
+        for(i=0; i<table.length; ++i){
+            var lucky = table[i].getElementsByClassName('lucky');
+            var lucky_time = table[i].getElementsByClassName('lucky_time');
+            var edited = $('#input_edit');
+            var is_edited = edited.children().length;
+            for(j=0; j<lucky.length; ++j){
+                var lucky_name = lucky[j].textContent;
+                var shift = lucky_time[j].textContent.split('-');
+                var set_array = table[i].getElementsByClassName(lucky_name)[0];
+                for(k=shift[0]; k<shift[1]; ++k){
+                    if(set_array){
+                        set_array.getElementsByClassName(k)[0].classList.add('active');
+                    }
+                }
+                if((is_edited != '0') && (i == '0')){
+                    var edit_lucky = edited[0].getElementsByClassName(`edit_${lucky_name}`)[0];
+                    edit_lucky.getElementsByClassName('start')[0].value = shift[0];
+                    edit_lucky.getElementsByClassName('end')[0].value = shift[1];
+                }
+            }
+        }
     });
 </script>
 </html>
